c18c95a29ae6341b75d64abd36eec1586d9badba
jakethecoder95 <95jacob07@gmail.com>
2024-04-11 16:01:47 -0700

    Enable I2C Pins

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
@@ -51,6 +53,7 @@ TIM_HandleTypeDef htim2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -233,6 +236,7 @@ int main(void) {
     /* Initialize all configured peripherals */
     MX_GPIO_Init();
     MX_TIM2_Init();
+    MX_I2C1_Init();
     /* USER CODE BEGIN 2 */
 
     HAL_TIM_Encoder_Start_IT(&htim2, TIM_CHANNEL_ALL);
@@ -258,13 +262,13 @@ int main(void) {
         if (!pomodoro.paused
                 && (pomodoro.mode == FOCUSING || pomodoro.mode == RESTING)) {
 
-            uint16_t target_time;
+            uint32_t target_time;
             if (pomodoro.mode == FOCUSING) {
-//			  target_time = pomodoro.focus * 60 * 1000;
-                target_time = pomodoro.focus * 1000;
+                target_time = pomodoro.focus * 60 * 1000;
+//                target_time = pomodoro.focus * 1000;
             } else {
-//			  target_time = pomodoro.rest * 60 * 1000;
-                target_time = pomodoro.rest * 1000;
+                target_time = pomodoro.rest * 60 * 1000;
+//                target_time = pomodoro.rest * 1000;
             }
 
             pomodoro.elapsed_time += curmilli - pomodoro.prev_time;
@@ -317,6 +321,38 @@ void SystemClock_Config(void) {
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
         Error_Handler();
     }
+}
+
+/**
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C1_Init(void) {
+
+    /* USER CODE BEGIN I2C1_Init 0 */
+
+    /* USER CODE END I2C1_Init 0 */
+
+    /* USER CODE BEGIN I2C1_Init 1 */
+
+    /* USER CODE END I2C1_Init 1 */
+    hi2c1.Instance = I2C1;
+    hi2c1.Init.ClockSpeed = 100000;
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN I2C1_Init 2 */
+
+    /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -380,6 +416,7 @@ static void MX_GPIO_Init(void) {
     __HAL_RCC_GPIOC_CLK_ENABLE();
     __HAL_RCC_GPIOD_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 
     /*Configure GPIO pin Output Level */
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
